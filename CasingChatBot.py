5a8a7eaec7a66659f0b29d1b9da79a5e1e709a4e
Rami812 <87784790+Rami812@users.noreply.github.com>
2025-07-03 18:10:38 -0400

    Update CasingChatBot.py

--- CasingChatBot.py
+++ CasingChatBot.py
@@ -1,5 +1,5 @@
 import streamlit as st
-pip install pypdf
+!pip install pypdf
 from pypdf import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import LlamaCppEmbeddings
