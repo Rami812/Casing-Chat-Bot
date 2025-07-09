8e90b0863044880d42accdba36096faec73a1d32
Rami812 <87784790+Rami812@users.noreply.github.com>
2025-07-08 21:16:44 -0400

    Update CasingChatBot.py

--- CasingChatBot.py
+++ CasingChatBot.py
@@ -386,7 +386,7 @@ def main():
         """)
 
 if __name__ == '__main__':
-    main()
+  main()
   __import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+  import sys
+  sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
